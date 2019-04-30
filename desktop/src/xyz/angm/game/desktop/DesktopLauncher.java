6cd531dcd6a82574f6a4a33045f156afb0c90521
Ellie Ang <git@angm.xyz>
2019-04-30 14:58:54 +0200

    Add some LWJGL configuration

--- desktop/src/xyz/angm/game/desktop/DesktopLauncher.java
+++ desktop/src/xyz/angm/game/desktop/DesktopLauncher.java
@@ -5,8 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import xyz.angm.game.Game;
 
 public class DesktopLauncher {
+
     public static void main (String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.backgroundFPS = 15;
+        config.resizable = true;
+        config.title = "GAME";
         new LwjglApplication(new Game(), config);
     }
 }
