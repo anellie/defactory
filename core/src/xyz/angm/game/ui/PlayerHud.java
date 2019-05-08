02fb3897134b910bbf5b053d9fc8c6e876c8b453
Ellie Ang <git@angm.xyz>
2019-05-08 16:12:41 +0200

    Nerf sprinting by implementing stamina

--- core/src/xyz/angm/game/ui/PlayerHud.java
+++ core/src/xyz/angm/game/ui/PlayerHud.java
@@ -4,12 +4,17 @@ import com.badlogic.gdx.scenes.scene2d.Group;
 import com.kotcrab.vis.ui.widget.VisProgressBar;
 
 import static xyz.angm.game.world.entities.Player.PLAYER_HEALTH;
+import static xyz.angm.game.world.entities.Player.PLAYER_STAMINA;
 
 /** The player HUD containing all GUI elements of the player, like health bar or inventory. */
 class PlayerHud extends Group {
 
+    private static final int BAR_WIDTH = 600;
+    private static final int BAR_HEIGHT = 15;
+
     private final GameScreen screen;
     private final VisProgressBar healthBar = new VisProgressBar(0, PLAYER_HEALTH, 1, false);
+    private final VisProgressBar staminaBar = new VisProgressBar(0, PLAYER_STAMINA, PLAYER_STAMINA / BAR_WIDTH, false);
 
     /** Construct a new HUD.
      * @param screen The screen the HUD will be a part of */
@@ -18,13 +23,18 @@ class PlayerHud extends Group {
         setPosition(0, 0);
 
         addActor(healthBar);
-        healthBar.setSize(600, 15);
+        healthBar.setSize(BAR_WIDTH, BAR_HEIGHT);
         healthBar.setPosition(0, 0);
+
+        addActor(staminaBar);
+        staminaBar.setSize(BAR_WIDTH, BAR_HEIGHT);
+        staminaBar.setPosition(0, BAR_HEIGHT);
     }
 
     @Override
     public void act(float delta) {
         super.act(delta);
         healthBar.setValue(screen.getWorld().getPlayer().getHealth());
+        staminaBar.setValue(screen.getWorld().getPlayer().getStamina());
     }
 }
--- core/src/xyz/angm/game/world/entities/Player.java
+++ core/src/xyz/angm/game/world/entities/Player.java
@@ -13,7 +13,12 @@ public class Player extends Entity {
 
     /** The maximum player health. */
     public static final int PLAYER_HEALTH = 20;
+    /** The maximum stamina. Sprinting depletes stamina; not sprinting refills */
+    public static final float PLAYER_STAMINA = 5f;
     private static final float SPRINT_MULTIPLIER = 2.5f;
+
+    private float stamina = PLAYER_STAMINA;
+    private boolean isSprinting = false;
 
     /** Constructs a Player. Requires AssetManager in Game to be ready. */
     public Player() {
@@ -25,7 +30,25 @@ public class Player extends Entity {
     /** Toggles the player sprinting. Sprinting causes the player to move faster.
      * @param sprint (false: Stop sprinting) (true: start sprinting) */
     public void sprint(boolean sprint) {
+        if (isSprinting == sprint) return;
+        isSprinting = sprint;
+
         if (sprint) movementMultiplier *= SPRINT_MULTIPLIER;
         else movementMultiplier /= SPRINT_MULTIPLIER;
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+
+        if (isSprinting) stamina -= delta;
+        else stamina += delta;
+
+        if (stamina < 0) sprint(false);
+        stamina = Math.max(0, Math.min(PLAYER_STAMINA, stamina));
+    }
+
+    public float getStamina() {
+        return stamina;
     }
 }
